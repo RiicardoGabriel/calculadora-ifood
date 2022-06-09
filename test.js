305476d5d996bb14b0c81bc63d196a7ae7da9463
Ricardo Gabriel <riicardogabriel12@gmail.com>
2022-06-09 18:43:23 -0300

    ajustes no botão

--- test.js
+++ test.js
@@ -1,19 +1,18 @@
 window.onload = function () {
-    function calculadora () {
-    document.querySelector("#btn").addEventListener('click', () => {
+function calculadora () {
+    document.querySelector("#inp").addEventListener('keyup', () => {
         const calculo = document.querySelector('#inp').value - document.querySelector('#inp').value * 17.5 / 100;
         const calcRounded = calculo.toFixed(2);
         document.querySelector("#valor").innerHTML = `Valor a receber: <b>R$ ${calcRounded}</b>`;
-        document.querySelector("#inp").value = '';
 })
 }
 
-document.addEventListener('keypress', function(e){
-    if(e.key == 'Enter'){
-        var btn = document.querySelector("#btn");
-        btn.click();
-    }
-  }, false);
-  
+function apagarInpt () {
+    document.querySelector("#btn").addEventListener('click', () => {
+        document.querySelector("#inp").value = '';
+    })
+}
+
 calculadora();
+apagarInpt();
 };
